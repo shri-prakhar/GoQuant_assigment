0963b4bbf91e2b9d3ae35780ed87a843ee5d4173
shri-prakhar <23f3000621@es.study.iitm.ac.in>
2026-01-07 11:26:33 +0530

    initial commit

---
+++ programs/goquant_assignment/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("A9JDc7TrKR5Qyot3W3t6UQaRz4CTgEURemuSUkWfP9hs");
+
+#[program]
+pub mod goquant_assignment {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
